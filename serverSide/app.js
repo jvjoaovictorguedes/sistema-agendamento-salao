58fc5fc74abcb46326a9c4ac0482e1a25e4213a7
Joao Victor <134668287+jvjoaovictorguedes@users.noreply.github.com>
2024-01-18 13:54:43 -0300

    Update app.js

--- serverSide/app.js
+++ serverSide/app.js
@@ -6,6 +6,15 @@ const port = process.env.PORT
 
 app.use(routes)
 
+app.use('/people', require('./src/routes/people.routes'));
+app.use('/peopleType', require('./src/routes/peopleType.routes'));
+app.use('/address', require('./src/routes/address.routes'));
+app.use('/procedure', require('./src/routes/procedure.routes'));
+app.use('/service', require('./src/routes/service.routes'));
+app.use('/procedureSchedule', require('./src/routes/procedureSchedule.routes'));
+app.use('/scheduling', require('./src/routes/scheduling.routes'));
+app.use('/procedureValue', require('./src/routes/procedureValue.routes'));
+
 app.listen(port, () => {
   console.log(`Servidor escutando a porta: ${port}`)
 })
