b9fad6e83b65ac2a4232cc3dcbf4144c5fb0b9bd
Joao Victor <134668287+jvjoaovictorguedes@users.noreply.github.com>
2024-01-19 17:59:37 -0300

    Update procedure.routes.js

--- serverSide/src/routes/procedure.routes.js
+++ serverSide/src/routes/procedure.routes.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const Procedure = require('../models/procedure');
 const Service = require('../models/service');
 
-router.post('/:id_service', async (req, res) => {
+router.post('/', async (req, res) => {
   try{
-    const { id_service } = req.params
+    const { id_service, procedure_name } = req.body
     const existingService = await Service.findByPk(id_service)
     if (!existingService) {
       return res.status(404).json({ error: true, message: 'Service not found.' });
     }
-    const procedure = await Procedure.create({
-      ...req.body,
-      id_service,
-    });
+    const procedure = await Procedure.create(id_service, procedure_name);
     res.json({ procedure });
   } catch (err) {
     res.json({ error: true, message: err.message });
@@ -33,6 +30,15 @@ router.get('/:id_service', async (req, res) => {
         id_service,
       },
     });
+    res.json({ procedure });
+  } catch (err) {
+    res.status(500).json({ error: true, message: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const procedure = await Procedure.findAll({});
     res.json({ procedure });
   } catch (err) {
     res.status(500).json({ error: true, message: err.message });
