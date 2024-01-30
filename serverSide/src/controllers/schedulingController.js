bc6880b8503b6d522ea301be6e0032c63658173b
Joao Victor <134668287+jvjoaovictorguedes@users.noreply.github.com>
2024-01-30 11:25:44 -0300

    Update schedulingController.js

--- serverSide/src/controllers/schedulingController.js
+++ serverSide/src/controllers/schedulingController.js
@@ -1,4 +1,6 @@
 const Scheduling = require("../models/scheduling");
+const Service = require("../models/service");
+const moment = require("moment");
 
 const InsertScheduling = async (req, res) => {
   try {
@@ -17,16 +19,60 @@ const InsertScheduling = async (req, res) => {
   }
 };
 
-const FindScheduling = async (req, res, next) => {
+const FindScheduling = async (__req, res) => {
   try {
     const findScheduling = await Scheduling.findAll();
     return res.json({ findScheduling });
-    next();
   } catch (error) {
     res.json({ error: true, message: error.message });
   }
 };
+
+const SumDate = async (__req, res) => {
+  try {
+    const findDateInitial = await Scheduling.findAll();
+
+    const datesAndTimes = findDateInitial.map((item) => ({
+      date: item.date,
+      time: item.time,
+    }));
+
+    const findDurations = await Service.findAll();
+
+    const durationsInMinutes = findDurations.map(({ service_duration }) => {
+      const convertToMinutes = (duration) => {
+        const [hours, minutes] = duration.split(":");
+        return parseInt(hours, 10) * 60 + parseInt(minutes, 10);
+      };
+      
+      return convertToMinutes(service_duration);
+    });
+    const nameService = findDurations.map((item => item.service_name));
+
+    const results = datesAndTimes.map(({ date, time }, index) => {
+      const datInit = moment(date, "YYYY-MM-DD").add(moment.duration(time));
+      const durationMinutes = durationsInMinutes[index];
+
+      const datFinal = datInit
+        .clone()
+        .add(durationMinutes, "minutes")
+        .toISOString();
+        
+
+      return {
+        datInit: datInit.toISOString(),
+        datFinal: datFinal,
+        
+      };
+    });
+    res.json({ results, nameService });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+};
+
 module.exports = {
   InsertScheduling,
   FindScheduling,
+  SumDate,
 };
